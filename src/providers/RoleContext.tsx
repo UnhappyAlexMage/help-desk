54537c13e4d8b7f42989a1e3a7e5b586602273b1
UnhappyAlex <alexandr.bro03@gmail.com>
2026-05-19 04:49:11 +0500

    Changed provider RoleContext

--- src/providers/RoleContext.tsx
+++ src/providers/RoleContext.tsx
@@ -2,14 +2,19 @@ import { createContext, useContext, useState, type ReactNode } from "react";
 import type { User } from "../entities/model/types.ts";
 
 type UserContextValue = {
-    currentUser: User | null,
+    currentUser: User,
     setCurrentUser: (user: User) => void;
 };
 
 const UserContext = createContext<UserContextValue | null>(null);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-    const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [currentUser, setCurrentUser] = useState<User>({
+        id: "user1",
+        fullName: "Иван Петров",
+        email: "ivan.petrov@company.com",
+        role: "employee",
+    });
 
     return(
         <UserContext.Provider value={{ currentUser, setCurrentUser }}>
