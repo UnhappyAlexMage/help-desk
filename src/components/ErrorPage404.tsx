2f4e778fb0f3668ce66f89202266feadff1e4f7a
UnhappyAlex <alexandr.bro03@gmail.com>
2026-05-19 13:35:08 +0500

    Fixed component ErrorPage404

--- src/components/ErrorPage404.tsx
+++ src/components/ErrorPage404.tsx
@@ -11,21 +11,21 @@ export default function ErrorPage404 () {
     return (
         <div className="bg-white  w-full flex items-center flex-col pt-14 px-4">
             <img src={image} alt="error-404" className="w-80 h-80 object-contain" />
-        <div className="flex flex-col items-center text-center max-w-md">
-            <div className="pb-5">
-                <h2 className="text-2xl font-serif text-red-600 mb-2">
-                    Ваша страница не найдена!
-                </h2>
-                <p className="text-gray-600 font-serif">
-                    Для перехода на домашнюю страницу нажмите кнопку ↓
-                </p>
+            <div className="flex flex-col items-center text-center max-w-md">
+                <div className="pb-5">
+                    <h2 className="text-2xl font-serif text-red-600 mb-2">
+                        Ваша страница не найдена!
+                    </h2>
+                    <p className="text-gray-600 font-serif">
+                        Для перехода на домашнюю страницу нажмите кнопку ↓
+                    </p>
+                </div>
+                <button className="bg-gray-500 hover:bg-gray-600 py-2 px-6 text-white font-bold text-lg rounded-lg transition-colors duration-200"
+                        onClick={handleClick}
+                    >
+                    Back to HomePage
+                </button>
             </div>
-            <button className="bg-gray-500 hover:bg-gray-600 py-2 px-6 text-white font-bold text-lg rounded-lg transition-colors duration-200"
-                    onClick={handleClick}
-                >
-                Back to HomePage
-            </button>
-        </div>
         </div>
     );
 };
