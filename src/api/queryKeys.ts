8ada607d585ab379396f92b0a057cd07e0ef3a30
UnhappyAlex <alexandr.bro03@gmail.com>
2026-06-10 22:55:07 +0500

    Fixed queryKeys

--- src/api/queryKeys.ts
+++ src/api/queryKeys.ts
@@ -1,8 +1,11 @@
 export const queryKeys = {
     tickets: {
         all: ["tickets"] as const,
-        detail: (ticketId: string) => ["tickets", ticketId] as const,
-        commnets: (ticketId: string) => ["tickets", ticketId, "comments"] as const,
+        detail: (ticketId: string | undefined) => ["tickets", ticketId] as const,
+    },
+    comments: {
+        all: ["commnets"] as const,
+        byTicket: (ticketId: string | undefined) => ["commnets", ticketId] as const,
     },
     users: {
         all: ["users"] as const,
