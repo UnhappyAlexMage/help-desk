af2d9943d9f23d085f8efdbcfd8709d0a72bde23
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-06-18 20:00:16 +0500

    Fixed function getUserNameById

--- src/shared/lib/getUserNameById.ts
+++ src/shared/lib/getUserNameById.ts
@@ -2,13 +2,9 @@ import { users } from "../../mock/data/users.ts";
 
 export const getUserNameById = ( userId?: string ) => {
   
-  if (!userId) {
-    return "Неизвестный User";
-  };
+  if (!userId) { return "Неизвестный User" };
 
-  const user = users.find(
-    (user) => user.id === userId
-  );
+  const user = users.find((user) => user.id === userId);
 
   console.log("FOUND USER:", user);
 
