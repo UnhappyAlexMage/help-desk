125b278e6f18a8ba897f2c5a72edf034296abe8d
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-09-04 23:57:54 +0500

    Added test file for user-context

---
+++ src/providers/useUser.ts
@@ -0,0 +1,11 @@
+// import { useContext } from "react";
+
+// export const useUser = () => {
+//     const contextUser = useContext(UserContext);
+
+//     if(!contextUser) {
+//         throw new Error("useUser must be used wethin UserContext");
+//     };
+
+//     return contextUser;
+// };
